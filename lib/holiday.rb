103d239cda70554cb57a886b7cf3ae18f6a36580
John Hill <john.m.hill91@gmail.com>
2019-03-28 02:31:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,6 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 holiday_hash.each do |season, holiday|
   puts "#{season.to_s.capitalize}:"
+  holiday.each do
 end
 
 def all_holidays_with_bbq(holiday_hash)
